57db4b167b64f446d77e11d86bdcd960bc3ecac5
mariselvan21 <mari20041999@gmail.com>
2022-07-18 10:53:12 +0530

    add files

---
+++ set1/push-method/push.js
@@ -0,0 +1,6 @@
+const foodItems=["Biriyani","White rice","pizza"]
+console.log("Food Items:"+foodItems);
+console.log("The length of the food items array:"+foodItems.push());
+foodItems.push("Chicken gravy","Mutton Gravy");
+console.log("Pushing some items in Food Items:"+foodItems);
+console.log("The length of the after pushing food items array:"+foodItems.push());
